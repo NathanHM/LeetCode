548bcdf5f1f3dafd2165f572e37a3f914fb996c3
NathanHM <NathanHM16@gmail.com>
2024-09-02 22:58:13 +0100

    Completed Longest Substring without Repetition challenge

---
+++ longestSubSstringWithoutRepeating.py
@@ -0,0 +1,24 @@
+s = "pwwkew"
+
+def lengthOfLongestSubstring(s):
+
+    chars = {}
+
+    start = 0
+    end = 0
+    diff = -1
+
+    for i in range(len(s)):
+        end = i
+        if not chars.get(s[i]):
+            chars[s[i]] = 0
+        chars[s[i]] += 1
+        while (chars[s[i]] > 1):
+            chars[s[start]] -= 1
+            start += 1
+        if end - start > diff:
+            diff = end - start 
+
+    return diff + 1
+
+print(lengthOfLongestSubstring(s))
