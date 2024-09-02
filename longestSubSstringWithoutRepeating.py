76c8d54f0cdfef48152abccd69f3dc8fedcf0268
NathanHM <NathanHM16@gmail.com>
2024-09-02 23:00:31 +0100

    Whitespace

--- longestSubSstringWithoutRepeating.py
+++ longestSubSstringWithoutRepeating.py
@@ -9,13 +9,18 @@ def lengthOfLongestSubstring(s):
     diff = -1
 
     for i in range(len(s)):
+        
         end = i
+
         if not chars.get(s[i]):
             chars[s[i]] = 0
+
         chars[s[i]] += 1
+
         while (chars[s[i]] > 1):
             chars[s[start]] -= 1
             start += 1
+
         if end - start > diff:
             diff = end - start 
 
